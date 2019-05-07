66a05a4677476e1cd2c1ce3330fa5078e63463a2
danielbaltes <db124@hdm.de>
2019-05-07 13:51:12 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	php/registrieren.php

--- php/registrieren.php
+++ php/registrieren.php
@@ -20,12 +20,12 @@ $pdo=new PDO('mysql:: host=mars.iuk.hdm-stuttgart.de;
 $db = $pdo->prepare("INSERT INTO gettobox_user_registrierung (Benutzer, Passwort, E-mail) 
                 VALUES (Benutzer, Passwort, E-mail");
 
-$pw = $_POST['psw'];
+$pw = $_POST['pw1'];
 $bn = $_POST['benutzer'];
 $ma = $_POST['email'];
 
 
-if (isset($_POST['registrieren'])) {
+if (isset($_POST['registreiren'])) {
     $hashPassword = password_hash($pw,PASSWORD_DEFAULT);
 
     $db->bindParam(':Benutzer', $_POST["benutzer"]);
@@ -33,11 +33,11 @@ if (isset($_POST['registrieren'])) {
     $db->bindParam(':E-mail', $_POST["email"]);
     if ($db->execute()) {
         echo "alles tight: " .$id=$pdo->lastInsertId();}
-    else(strpos($ma , "@") !==false)
+    elseif (strpos($ma , "@") !==false)
     {echo 'E-Mail falsch!';}
     elseif ($check = mssql_free_statement("SELECT benutzer from gettobox_user_registrierung WHERE benutzer = $bn"));
-            {echo 'Benutzer existiert bereits';}
-    else {echo 'Fehler bei der Registrierung'; die(); }
+            {echo 'Benutzer exisitert bereits';}
+    else {echo 'Fehler bei der Registierung'; die(); }
 
 
 
